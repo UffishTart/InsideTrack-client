6f5060cb9906ee1c3347ef7f60fc2baada19b526
Dan Needham <dan.needham3@gmail.com>
2018-11-15 20:34:39 -0500

    added redux to boilerplate

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import AuthForm from './components/auth-form'
+import AuthForm, {Login, Signup} from './components/auth-form'
 export default class App extends React.Component {
   render() {
     return (
