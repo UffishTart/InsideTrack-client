2ba658a7da47a878d74e131f0d6ded93691b2f36
Patrick Aloia <patrickalexanderaloia@gmail.com>
2018-11-19 18:45:05 -0500

    added horse

--- components/HorseComponent.js
+++ components/HorseComponent.js
@@ -2,13 +2,15 @@
 //Will require some type of Three js import -- loading in a JSON object
 //Need for resizing probably depending on component? We'll see
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 // create a component
 class HorseComponent extends Component {
   render() {
     return (
-      <Text>HORSE</Text>
+      // <Text>Your Horse!</Text>
+      <Image style={{ width: 200, height: 200 }}
+        source={require('../assets/Standard_Horse.jpg')}></Image>
     );
   }
 }
