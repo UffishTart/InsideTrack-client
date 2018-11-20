1f38782e9c8bdd1a7f9f5eaa8636bd020098d389
ryhoneydew <lucia.yang88@gmail.com>
2018-11-20 12:43:04 -0500

    add Finduser

--- store/user.js
+++ store/user.js
@@ -1,66 +1,66 @@
-import axios from 'axios'
-const server = 'https://inside-track-server-boil.herokuapp.com'
-import {onSignIn, onSignOut} from '../navigation/AsyncStorageAuth'
+import axios from "axios";
+const server = "https://inside-track-server-boil.herokuapp.com";
+import { onSignIn, onSignOut } from "../navigation/AsyncStorageAuth";
 
 // import history from '../history'
 
 //  * ACTION TYPES
 
-const GET_USER = 'GET_USER'
-const REMOVE_USER = 'REMOVE_USER'
+const GET_USER = "GET_USER";
+const REMOVE_USER = "REMOVE_USER";
 
 //  * INITIAL STATE
-const defaultUser = {}
+const defaultUser = {};
 
 //  * ACTION CREATORS
-export const getUser = user => ({ type: GET_USER, user })
-const removeUser = () => ({ type: REMOVE_USER })
+export const getUser = user => ({ type: GET_USER, user });
+const removeUser = () => ({ type: REMOVE_USER });
 
 // * THUNK CREATORS
 export const me = () => async dispatch => {
   try {
-    const res = await axios.get(`${server}/auth/me`)
-    dispatch(getUser(res.data || defaultUser))
+    const res = await axios.get(`${server}/auth/me`);
+    dispatch(getUser(res.data || defaultUser));
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 export const auth = (email, password, method) => async dispatch => {
-  let res
-  console.log('should say login', method)
+  let res;
+  console.log("should say login", method);
   try {
-    res = await axios.post(`${server}/auth/${method}`, { email, password })
+    res = await axios.post(`${server}/auth/${method}`, { email, password });
   } catch (authError) {
-    return dispatch(getUser({ error: authError }))
+    return dispatch(getUser({ error: authError }));
   }
 
   try {
-    dispatch(getUser(res.data))
-    onSignIn(res.data)
+    dispatch(getUser(res.data));
+    onSignIn(res.data);
   } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
+    console.error(dispatchOrHistoryErr);
   }
-}
+};
 
 export const logout = () => async dispatch => {
   try {
-    await axios.post(`${server}/auth/logout`)
-    dispatch(removeUser())
-    onSignOut()
+    await axios.post(`${server}/auth/logout`);
+    dispatch(removeUser());
+    onSignOut();
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 // * REDUCER
-export default function (state = defaultUser, action) {
+export default function(state = defaultUser, action) {
   switch (action.type) {
     case GET_USER:
-      return action.user
+      return action.user;
     case REMOVE_USER:
-      return defaultUser
+      return defaultUser;
     default:
-      return state
+      return state;
   }
 }
