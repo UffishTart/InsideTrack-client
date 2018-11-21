a50395ebb1ff5d5f51eff9cdcc4f8f5ebc29392a
Patrick Aloia <patrickalexanderaloia@gmail.com>
2018-11-21 13:17:59 -0500

    initial commit for pending-races-components

---
+++ components/PendingRacesList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import PendingRacesListItem from './PendingRacesListItem';
+
+const PendingRacesList = props => {
+  const { user, races, isOwnerBool } = props;
+  return (
+    <View
+      style={
+        isOwnerBool
+          ? [styles.flexCont, { backgroundColor: '#D9AFAF' }]
+          : [styles.flexCont, { backgroundColor: '#A3DDFA' }]
+      }
+    >
+      <ScrollView>
+        {!!races.length &&
+          races
+            .filter(race => {
+              return race.isOwner ? race.isOwner !== isOwnerBool : 'null';
+            })
+            .map(race => {
+              return (
+                <PendingRacesListItem
+                  key={race.raceId}
+                  user={user}
+                  race={race}
+                />
+              );
+            })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default PendingRacesList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  flexCont: {
+    flex: 1,
+  },
+});
