73fdc13fe9e1f28bc72ba900d7a378a399809a36
Patrick Aloia <patrickalexanderaloia@gmail.com>
2018-11-28 15:28:00 -0500

    more styling of buttongs

--- screens/tabs/ProfileScreen.js
+++ screens/tabs/ProfileScreen.js
@@ -1,10 +1,10 @@
 //import liraries
 import React, { Component } from "react";
 import HorseComponent from "../../components/HorseComponent";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { StyleSheet } from "react-native";
 import FindUsers from "../../components/FindUsers";
-import { Container, Header, Left, Body, Right, Icon, Title } from 'native-base';
-import HorseStore from "../pop-up-screens/HorseStore"
+import { Container, Header, Left, Body, Right, Icon, Title } from "native-base";
+import HorseStore from "../pop-up-screens/HorseStore";
 
 // create a component
 export default class ProfileScreen extends Component {
