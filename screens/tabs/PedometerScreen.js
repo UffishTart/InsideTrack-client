e56a4db3cbec6d7f079d181a0847105c4b581eb2
ryhoneydew <lucia.yang88@gmail.com>
2018-11-29 23:00:05 -0500

    Add d3 visualization

--- screens/tabs/PedometerScreen.js
+++ screens/tabs/PedometerScreen.js
@@ -285,11 +285,15 @@ class PedometerSensor extends React.Component {
           >
             <Track
               data={racingUserData}
+              selectX={datum => datum.Improvement}
+              selectY={idx => idx}
               steps={this.state.stepCountDuringGame}
               width={Dimensions.get("window").width}
               height={Dimensions.get("window").height}
               user={this.props.user}
-              usersInRace={this.props.singleRaceUser.filter(obj => obj.raceId === this.props.raceId)}
+              usersInRace={this.props.singleRaceUser.filter(
+                obj => obj.raceId === this.props.raceId
+              )}
             />
 
             <Button
